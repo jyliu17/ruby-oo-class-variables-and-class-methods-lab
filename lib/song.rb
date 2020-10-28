b9f34d42250df60af4694ca57c8cc4bb4ebd16fa
jyliu17 <jyliu17@gmail.com>
2020-10-28 19:42:15 -0400

    done

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,19 @@
 class Song
 
-    attr_accessor :name, :artist, :genre
-
+    attr_reader :name, :artist, :genre
     @@count = 0
-
     @@artists = []
     @@genres = []
 
     def initialize(name, artist, genre)
-     @name = name
-     @artist = artist
-     @genre = genre
-     @@count += 1
-     @@artists << artist
-     @@genres << genre
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
     end
+
 
     def self.count
         @@count
@@ -31,30 +30,25 @@ class Song
     def self.genre_count
         genre_count = {}
         @@genres.each do |genre|
-            if !genre_count[genre] 
-                genre_count[genre]= 1
-            else
-              genre_count[genre] += 1
-            end
+          if genre_count[genre]
+            genre_count[genre] += 1 
+          else
+            genre_count[genre] = 1
+          end
         end
         genre_count
       end
-
-    def self.artist_count
+        
+      def self.artist_count
         artist_count = {}
         @@artists.each do |artist|
-            if !artist_count[artist]
-                artist_count[artist] = 1
-            else
-                artist_count[artist] += 1
-            end
-
+          if artist_count[artist]
+            artist_count[artist] += 1 
+          else
+            artist_count[artist] = 1
+          end
         end
-
         artist_count
-    end
-
-
-        
+      end
 
 end
